2e505bfaccd4f8108292eb6a4b15e292cc464a86
Soufiane El Bourakkadi <souel-bo@e1r6p9.1337.ma>
2024-11-19 20:56:07 +0100

    sss

--- get_next_line.h
+++ get_next_line.h
@@ -1,20 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: souel-bo <souel-bo@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 16:13:32 by souel-bo          #+#    #+#             */
+/*   Updated: 2024/11/19 18:48:57 by souel-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
 
 #ifndef BUFFER_SIZE
 #define BUFFER_SIZE 1024
-
 #endif
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-char *get_next_line(int fd);
-size_t ft_strlen(const char *s);
-size_t ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char *ft_strjoin(char const *s1, char const *s2);
-size_t ft_strlcat(char *dest, const char *src, size_t dstsize);
-char *ft_strchr(const char *s, int c);
+char	*get_next_line(int fd);
+size_t  ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char    *ft_strjoin(char const *s1, char const *s2);
 #endif
